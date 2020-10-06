267fdd28dcfb74a5d3c167253f2fe74f4dd538c0
parakeet15 <kaisei_19n1102563@nnn.ed.jp>
2020-10-06 20:31:03 +0900

    async/await を用いて同期処理に変更

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 'use strict';
 const fs = require('fs');
 const fileName = './test.txt';
-for (let count = 0; count < 30; count++) {
-    fs.appendFile(fileName, 'おはようございます\n', 'utf8', () => {});
-    fs.appendFile(fileName, 'こんにちは\n', 'utf8', () => {});
-    fs.appendFile(fileName, 'こんばんは\n', 'utf8', () => {});
-}
+(async () => {
+    for (let count = 0; count < 30; count++) {
+        await fs.appendFile(fileName, 'おはようございます\n', 'utf8', () => { });
+        await fs.appendFile(fileName, 'こんにちは\n', 'utf8', () => { });
+        await fs.appendFile(fileName, 'こんばんは\n', 'utf8', () => { });
+    }
+})();
